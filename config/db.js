70528172fa9a9cfb047790d9a0b0e85808e24e0f
Vikram S <vikram.senthil@outlook.com>
2020-10-21 10:28:18 +0530

    Refactored Mongo promises with async await

--- config/db.js
+++ config/db.js
@@ -5,20 +5,20 @@ const config = require("config");
 const db = config.get("mongoURI");
 
 //mongoose returns promises.
-
-const connectDB = () => {
-  mongoose
-    .connect(db, {
+const connectDB = async () => {
+  //try catch with async await.
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
-    })
-    .then(() => console.log("MongoDB Connected"))
-    .catch((err) => {
-      console.error(err.message);
-      process.exit(1); //exiting with failure
     });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 //catch then syntax
