81c37935c0cf7a2818f965adb43de7d824ee154d
Vikram S <vikram.senthil@outlook.com>
2020-10-21 15:29:50 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ router.post(
   "/",
   [
     //Express-validation
-    //for checking the input with particular condition.
+    //for checking the input with particular condition. Validating the entry
     check("name", "Please add a name").not().isEmpty(),
     check("email", "Please include a valid email").isEmail(),
     check(
@@ -19,13 +19,40 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       //sends the array of errors with the errors object.
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("passed");
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email }); //email is actually email:email
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+      //creating new instance of the user
+      user = new User({
+        name, //name:name
+        email,
+        password,
+      });
+
+      //hashing the password using bcrypt -- all this lines return promises so we use await
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("User saved");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
