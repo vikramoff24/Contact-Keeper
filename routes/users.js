007a2988731da952d3fa6c697fbe28efba2b9d65
Vikram S <vikram.senthil@outlook.com>
2020-10-21 18:58:16 +0530

    Hashing of password

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator"); //from Express validator
+const bcrypt = require("bcryptjs");
 
+//Model
 const User = require("../models/User");
+
 // @route POST api/users
 // @desc Register a user
 // @access Public
-
 router.post(
   "/",
   [
