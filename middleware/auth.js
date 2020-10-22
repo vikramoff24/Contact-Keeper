ea318690aff5da8ccbee950ce263e093bfee1e5b
Vikram S <vikram.senthil@outlook.com>
2020-10-22 14:47:51 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,10 +2,10 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-//middleware
+//middleware function
 module.exports = function (req, res, next) {
   // Get token from header
-  const token = req.header("x-auth-token");
+  const token = req.header("x-auth-token"); //this value comes from the header property name which contains the token.
 
   // Check if not token
   if (!token) {
@@ -16,7 +16,7 @@ module.exports = function (req, res, next) {
     //decoded payload object of token
     const decoded = jwt.verify(token, config.get("jwtSecret"));
 
-    req.user = decoded.user;
+    req.user = decoded.user; //this will asgin the vaule os req object of the protected route which runs the middle ware.
     next();
   } catch (err) {
     res.status(401).json({ msg: "Token is not valid" });
