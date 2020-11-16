9208b5d21ae6f12432d5def02dfbd39db7522ef8
Vikram S <vikram.senthil@outlook.com>
2020-11-16 13:46:20 +0530

    about.js update

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="container">
+      <h1>About This App</h1>
+      <p className="my-1">This is a Full stack App for keeping contacts</p>
+      <p className="bg-dark p">
+        <strong>Version: </strong> 1.0.0
+      </p>
+    </div>
+  );
 };
 export default About;
