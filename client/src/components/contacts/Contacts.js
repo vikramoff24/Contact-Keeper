bc6046275e116410bd5a7675e5d07a531b023392
Vikram S <vikram.senthil@outlook.com>
2020-11-18 10:06:31 +0530

    Contacts and ContactItem

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,6 @@
+import React, { Fragment, useContext } from "react";
+import ContactContext from "../../context/contact/contactContext";
+
+export const Contacts = () => {
+  return <div></div>;
+};
