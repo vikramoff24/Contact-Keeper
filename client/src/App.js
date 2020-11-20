c123f643d2c31a7c8ff74a330bf62c2f4e811e71
Vikram S <vikram.senthil@outlook.com>
2020-11-21 00:10:21 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ const App = () => {
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
+                <Route exact path="/register" component={Register} />
               </Switch>
             </div>
           </Fragment>
