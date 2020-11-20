754762c59b2bb8b7fd79fb74b1c07e9cd05347c2
Vikram S <vikram.senthil@outlook.com>
2020-11-21 00:07:05 +0530

    Update Register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -7,11 +7,54 @@ const Register = () => {
     password: "",
     password2: "",
   });
+
+  const { name, email, password, password2 } = user;
+
+  const onChange = (e) => {
+    setUser({ ...user, [e.targer.name]: e.target.value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Registered");
+  };
   return (
     <div className="form-container">
       <h1>
         Account <span className="text-primary">Register</span>
       </h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" value={name} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="email" value={email} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password2">Confirm Password</label>
+          <input
+            type="password2"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+          />
+        </div>
+        <input
+          type="submit"
+          value="Register"
+          className="btn btn-primary btn-block"
+        />
+      </form>
     </div>
   );
 };
