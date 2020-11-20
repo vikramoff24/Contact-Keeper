e0e0f81b6a22c30586920107a707052300da062b
Vikram S <vikram.senthil@outlook.com>
2020-11-20 18:42:06 +0530

    ADDED Types for authentication

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useContext } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactContext from "../../context/contact/contactContext";
 import ContactItems from "./ContactItems";
 const Contacts = () => {
@@ -10,13 +11,19 @@ const Contacts = () => {
   }
   return (
     <Fragment>
-      {filtered !== null
-        ? filtered.map((contact) => (
-            <ContactItems contact={contact}></ContactItems>
-          ))
-        : contacts.map((contact) => (
-            <ContactItems contact={contact}></ContactItems>
+      {filtered !== null ? (
+        filtered.map((contact) => (
+          <ContactItems key={contact.id} contact={contact} />
+        ))
+      ) : (
+        <TransitionGroup>
+          {contacts.map((contact) => (
+          <CSSTransition key={contact.id} timeout={500} classNames="item">
+            <ContactItems key={contact.id} contact={contact} />
+          </CSSTransition>
           ))}
+        </TransitionGroup>
+      )}
     </Fragment>
   );
 };
