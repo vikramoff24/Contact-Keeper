12e1de6b60d9eb76c0ce07cf498340221745f495
Vikram S <vikram.senthil@outlook.com>
2020-11-21 12:36:30 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,9 @@ import { Fragment } from "react";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import ContactState from "./context/contact/ContactState";
-import AuthState from "./context/auth/authState";
+import AuthState from "./context/auth/AuthState";
 const App = () => {
   return (
     <AuthState>
@@ -19,6 +20,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </Fragment>
