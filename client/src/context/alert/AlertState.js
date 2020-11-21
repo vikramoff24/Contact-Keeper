8f8e648c2c0fd86befcad6d547afc6c479ac53cb
Vikram S <vikram.senthil@outlook.com>
2020-11-21 23:14:26 +0530

    Update AlertState.js

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -5,39 +5,29 @@ import alertReducer from "./alertReducer";
 import { SET_ALERT, REMOVE_ALERT } from "../types";
 
 const AlertState = (props) => {
-  const intialState = []
-}
+  const intialState = [];
 
   const [state, dispatch] = useReducer(alertReducer, intialState);
   //actions
   // Set Alert
 
-  const setAlert= (msg,type,timeout=5000)=>
-  {
-      const id=uuid();
-      dispatch({type:SET_ALERT,payload:{msg,type,id}});
+  const setAlert = (msg, type, timeout = 5000) => {
+    const id = uuid();
+    dispatch({ type: SET_ALERT, payload: { msg, type, id } });
 
-      setTimeout(()=>dispatch({type:REMOVE_ALERT,payload:id}),timeout)
-  }
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+  };
   //wrap entire application with context in App.js file.
   return (
-    <ContactContext.Provider
+    <AlertContext.Provider
       value={{
-        contacts: state.contacts,
-        current: state.current,
-        filtered: state.filtered,
-        addContact,
-        deleteContact,
-        setCurrent,
-        clearCurrent,
-        updateContact,
-        filterContacts,
-        clearFilter,
+        alerts: state,
+        setAlert,
       }}
     >
       {props.children}
-    </ContactContext.Provider>
+    </AlertContext.Provider>
   );
 };
 
-export default ContactState;
+export default AlertState;
