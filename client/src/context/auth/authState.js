eaafb3a9274434423fb13079f6b14e06040255d6
Vikram S <vikram.senthil@outlook.com>
2020-11-22 13:37:34 +0530

    authentication

--- client/src/context/auth/authState.js
+++ client/src/context/auth/authState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-
+import axios from "axios";
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
 import {
@@ -28,7 +28,20 @@ const AuthState = (props) => {
   // Load User
 
   // Register User
-
+  const register = async (FormData) => {
+    //type of data sent
+    const config = {
+      header: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post("/api/users", formData, config);
+      dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+    } catch (err) {
+      dispatch({ type: REGISTER_FAIL, payload: err.response.data.msg });
+    }
+  };
   // Login User
 
   // Logout
