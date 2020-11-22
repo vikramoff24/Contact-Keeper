862c2499a642e7ac32a9879d26a464c58b03b59e
Vikram S <vikram.senthil@outlook.com>
2020-11-22 23:51:08 +0530

    Added Logged in user name in navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,7 +4,30 @@ import { Link } from "react-router-dom";
 import AuthContext from "../../context/auth/authContext";
 const Navbar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
-  const { user, isAuthenticated } = authContext;
+  const { user, isAuthenticated, logout } = authContext;
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a href="#!">
+          <i className="fas fa-sign-out-alt"></i>
+          <span classNam="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className="navbar bg-primary">
       <h1>
@@ -12,18 +35,8 @@ const Navbar = ({ title, icon }) => {
         {title}
       </h1>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
+        {/* rendering combination of jsx based on condition  */}
+        {isAuthenticated ? authLinks : guestLinks}
       </ul>
     </div>
   );
