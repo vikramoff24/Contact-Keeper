7fc27c9dc849563f2738b5e93024e4eb4ce7bace
Vikram S <vikram.senthil@outlook.com>
2020-11-23 13:01:29 +0530

    PrivateRoute Implemented

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,3 +1,5 @@
+//private route component 
+
 import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
 import AuthContext from "../../context/auth/authContext";
@@ -5,7 +7,7 @@ import AuthContext from "../../context/auth/authContext";
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const authContext = useContext(AuthContext);
   const { isAuthenticated, loading } = authContext;
-  return(<Route {...rest} render={props=>!isAuthenticated&&!loading ?<Redirect to="/login"/>:<Componen {...props}} )
+  return(<Route {...rest} render={props=>!isAuthenticated&&!loading ?<Redirect to="/login"/>:<Component {...props}} )
 };
 
 export default PrivateRoute;
